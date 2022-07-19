7d5d17eae078f28bab272705b387b5b0e0dbd66a
iamdiegosanches <dgsanches3@gmail.com>
2022-07-19 15:22:37 -0300

    Role implementation

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -12,7 +12,6 @@ public class ParkingControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
-		System.out.println(new BCryptPasswordEncoder().encode("senha123"));
 	}
 
 	@GetMapping("/")
